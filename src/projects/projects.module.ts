106450840569c32e1bd0e9c5ee32777881cdbb23
Leeyah-123 <junaidaaliyah260@gmail.com>
2026-01-13 20:13:55 +0100

    feat: add NotificationsModule import to projects module for notification functionality integration

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 import { ActivitiesModule } from '../activities/activities.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { ProjectApplicationsService } from './project-applications.service';
@@ -8,7 +9,7 @@ import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 
 @Module({
-  imports: [WalletModule, ActivitiesModule],
+  imports: [WalletModule, ActivitiesModule, NotificationsModule],
   controllers: [ProjectsController],
   providers: [
     ProjectsService,
